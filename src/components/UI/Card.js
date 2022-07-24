6f8b7c0384961deb5def4acb7664037e853640e1
Ishika <agishika024@gmail.com>
2022-07-24 15:37:39 +0530

    my first commit

---
+++ src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+function Card(props){
+    const classs = 'card ' + props.className;
+
+    return (
+        <div className ={classs}>{props.children}</div>
+    );
+}
+
+export default Card;
